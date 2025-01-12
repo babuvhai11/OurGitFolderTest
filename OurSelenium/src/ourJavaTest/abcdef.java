4bbf2abd6aabfe3a28f1957490399e0641515402
Luva <jassitazrin@gmail.com>
2025-01-12 13:03:10 -0500

    Payment method processing test

---
+++ OurSelenium/src/ourJavaTest/abcdef.java
@@ -0,0 +1,11 @@
+package ourJavaTest;
+
+public class abcdef {
+
+	public static void main(String[] args) {
+		System.out.println("A");
+		System.out.println("B");
+		System.out.println("C");
+	}
+
+}
