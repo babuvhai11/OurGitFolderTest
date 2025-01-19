b4f84bcc4f5d87be88dbbc2ad2d3e35d45cda45f
Luva <jassitazrin@gmail.com>
2025-01-19 12:37:12 -0500

    Code from Mou

--- OurSelenium/src/ourJavaTest/abcdef.java
+++ OurSelenium/src/ourJavaTest/abcdef.java
@@ -4,9 +4,8 @@ public class abcdef {
 
 	public static void main(String[] args) {
 		System.out.println("A");
-		System.out.println("B");
-		System.out.println("C");
 		System.out.println("D");
+		System.out.println("X");
 	}
 
 }
